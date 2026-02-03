48e92bd9c87048a746e44737980bf99a42e55fb7
Vivaan <87912804+VivaanCode@users.noreply.github.com>
2026-02-02 18:26:12 -0800

    periodic commit

--- main.py
+++ main.py
@@ -6,32 +6,45 @@ import sqlite3
 
 
 def sqlConnect():
-    c = sqlite3.cect('db.sqlite3')
+    c = sqlite3.connect('db.sqlite3')
     c.row_factory = sqlite3.Row
+    print("connected to database")
+    print(c)
     return c
 
 def sqlInit():
     with sqlConnect() as c:
         c.execute('CREATE TABLE IF NOT EXISTS ushort_links (key TEXT PRIMARY KEY, value TEXT)')
+        print("created table successfully")
+        print(c)
 
 def sqlGet(key):
     with sqlConnect() as c:
         row = c.execute('SELECT value FROM ushort_links WHERE key = ?', (key,)).fetchone()
+        print("got row successfully")
+        print(c)
+        print(row)
         return row[0] if row else None
 
 def sqlSet(key, value):
     with sqlConnect() as c:
         c.execute('INSERT OR REPLACE INTO ushort_links (key, value) VALUES (?, ?)', (key, value))
         c.commit()
+        print("set row successfully")
+        print(c)
 
 def sqlClear():
     with sqlConnect() as c:
         c.execute('DELETE FROM ushort_links')
         c.commit()
+        print("cleared database")
+        print(c)
 
 sqlInit()
 
+#admin_code = os.getenv("ADMIN_CODE")
 admin_code = "test"
+
 
 
 def create_short_id_name():
@@ -74,11 +87,14 @@ def api_create():
 
   return redirect("/created?id="+id)
 
-@app.route('/admin/cleardb')
+@app.route('/admin/cleardb', methods=['POST'])
 def admin():
-  if request.form.get("admincode") == admin_code:
-    sqlClear()
-    return '', 200
+  try:
+    if request.form.get("admincode") == admin_code:
+      sqlClear()
+      return '', 200
+  except Exception as e:
+    return render_template("incorrect_admin_code.html")
 
 @app.errorhandler(400)
 def bad_request(code):
